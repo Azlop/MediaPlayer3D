41ebbb6245e4a1cb2de49a9fc223bc051d7366e4
Luis Carta <luis.carta@gmail.com>
2014-04-22 22:53:47 +0100

    Search by title, artist and genre -> Returns indexes

--- search_test/searchindex.js
+++ search_test/searchindex.js
@@ -1,3 +1,6 @@
+// Referência: http://dzone.com/snippets/simple-javascriptxml-based
+// Maior parte do código proveniente do website acima, com algumas modificaçoes nossas.
+
 window.onload = loadIndex;
 
 function loadIndex() { // load indexfile
@@ -18,23 +21,47 @@ function searchIndex() { // search the index.xml (duh!)
     if (!xmlDoc) {
         loadIndex();
     }
-    // get the search term from a form field with id 'searchme'
 
+    var tag;
+    if(document.getElementById('titulo').checked){
+        tag = "titulo";
+    } else if(document.getElementById('artista').checked){
+        tag = "artista";
+    } else if(document.getElementById('genero').checked){
+        tag = "genero";
+    }
+
+    // get the search term from a form field with id 'searchme'
     var searchterm = document.getElementById("searchme").value;
-    var titulo = xmlDoc.getElementsByTagName("titulo");
+    var details = xmlDoc.getElementsByTagName("music");
     results = new Array;
     if (searchterm.length < 3) {
         alert("Enter at least three characters");
     } else {
-        for (var i = 0; i < titulo.length; i++) {
+        for(var i = 0; i < details.length; i++){
+            if(details[i].childNodes){
+                for (var j = 0; j < details[i].childNodes.length; j++) {
+                    var detail = details[i].childNodes[j];
+                    if (detail.nodeName === tag) { // <- escolher a tag dos valores
+                        var name = detail.lastChild.nodeValue;
+                        var exp = new RegExp(searchterm, "i");
+                        if (name.match(exp) != null) {
+                            //alert("details node " + (i + 1) + ": " + detail.nodeName + " = " + detail.lastChild.nodeValue);
+                            results.push(i);
+                        }
+                    }
+                }
+            }
+        }
+        /*for (var i = 0; i < titulo.length; i++) {
             // see if the XML entry matches the search term,
             // and (if so) store it in an array
             var name1 = titulo[i].lastChild.nodeValue;
-            var exp1 = new RegExp(searchterm,"i");
-            if ( name1.match(exp1) != null) {
+            var exp1 = new RegExp(searchterm, "i");
+            if (name1.match(exp1) != null) {
                 results.push(titulo[i]);
             }
-        }
+        }*/
         // send the results to another function that displays them to the user
         showResults(results, searchterm);
     }
@@ -56,7 +83,7 @@ function showResults(results, searchterm) {
         resultshere.appendChild(list);
         for (var i=0;i<results.length;i++) {
             var listitem = document.createElement("li");
-            var item = document.createTextNode(results[i].lastChild.nodeValue);
+            var item = document.createTextNode(results[i]/*.lastChild.nodeValue*/);
             list.appendChild(listitem);
             listitem.appendChild(item);
         }
